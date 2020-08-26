b3c0b5780c1196de2b06cb92af9db129a49edec4
Mala-Patke <66596286+Mala-Patke@users.noreply.github.com>
2020-08-26 16:32:17 -0700

    Added Quotebook feature

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Client } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
 const LowDBProvider = require('./api/LowDBProvider.js');
 const path = require('path');
 const secret = require('./secret.json');
@@ -33,5 +34,52 @@ client.registry
     .registerDefaultGroups()
     .registerDefaultCommands({ help: false })
     .registerCommandsIn(path.join(__dirname, 'commands'));
+
+function sendEmbed(message){
+    let embed = new MessageEmbed()
+      .setTitle(`"${message.content}"`)
+      .addField("Said by", message.author, true)
+      .addField("On", message.createdAt, true)
+      .addField("In channel", message.channel, true)
+      .addField("Message link", message.url, true)
+      .setThumbnail("https://imgur.com/cNecthw.png")
+      .setFooter(message.id);
+    message.channel.send(embed);
+}
+
+client.on('messageReactionAdd', (reaction, user) => {
+    if(reaction.partial){
+        try {
+            await reaction.fetch()
+        }catch(error) {
+            console.log('Something went wrong: ', error);
+            return;
+        }
+    }
+
+    if(reaction.message.partial){
+        try {
+            await reaction.message.fetch()
+        }catch(error) {
+            console.log('Something went wrong: ', error);
+            return;
+        }
+    }
+    
+    let channel = reaction.message.guild.channels.cache.find(ch => ch.name.split("-").includes("quotebook"));
+    if(!channel) reaction.message.guild.channels.create()
+    
+    if(reaction.count >= 4 && reaction.emoji.id == '687164344261148696'){
+      try{
+        let fetch = await channel.messages.fetch({limit: 100});
+        let dupCheck = fetch.find(m => m.embeds[0].footer.text.startsWith(reaction.message.id));
+        if(!dupCheck){
+            sendEmbed(reaction.message);
+        }
+      }catch {
+        sendEmbed(reaction.message);
+      }
+    }
+}
 
 client.login(secret.client_tokens[secret.production ? 'production' : 'development']);
